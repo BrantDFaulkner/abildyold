f5e8ebcfb7e5281e560cf53c02a3338c9ac78b35
BrantDFaulkner <brantdfaulkner@gmail.com>
2016-03-24 12:33:45 -0500

    modify registrations controller; add jumbotron to welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :requests
   resources :goals
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
   # resources :users do
   resources :participations
